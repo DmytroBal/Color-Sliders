0deeae853a696f8064f1293025e28b56188cc3a1
Dmytro <mr.dmytrobal@gmail.com>
2022-08-09 18:21:19 +0300

    Add outlets

--- Color Sliders/ViewController.swift
+++ Color Sliders/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 20
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        
+        redSlider.value = 20
+        greenSlider.value = 80
+        blueSlider.value = 160
+        
     }
 
+    // Change colors with sliders
+    
 
 }
 
