f761c534c8ace91ac36c6c1cdfdf903439286478
Dmytro <mr.dmytrobal@gmail.com>
2022-08-10 16:40:29 +0300

    Add main screen logic

--- Color Sliders/ViewController.swift
+++ Color Sliders/ViewController.swift
@@ -27,14 +27,53 @@ class ViewController: UIViewController {
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         
-        redSlider.value = 20
-        greenSlider.value = 80
-        blueSlider.value = 160
+        redSlider.value = 30
+        greenSlider.value = 60
+        blueSlider.value = 120
         
+        setColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
     }
 
     // Change colors with sliders
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        
+        switch sender.tag {
+        case 0: redLabel.text = string(from: sender)
+        case 1: greenLabel.text = string(from: sender)
+        case 2: blueLabel.text = string(from: sender)
+        default: break
+        }
+    }
     
+    // Color view
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red:
+                CGFloat(redSlider.value) / 255,
+            green:
+                CGFloat(greenSlider.value) / 255,
+            blue:
+                CGFloat(blueSlider.value) / 255,
+            alpha: 1)
+    }
 
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label.tag {
+            case 0: redLabel.text = string(from: redSlider)
+            case 1: greenLabel.text = string(from: greenSlider)
+            case 2: blueLabel.text = string(from: blueSlider)
+            default: break
+            }
+        }
+    }
+    
+    // RGB Values
+    private func string(from slider: UISlider) -> String {
+        String(Int(slider.value))
+    }
+    
 }
 
